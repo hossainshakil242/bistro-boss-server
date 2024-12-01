7b00cf14f19a6d4bb6f8823a8d6d69b3a4cd2eb4
hossainshakil242 <hossainshakil242@gmail.com>
2024-12-01 12:12:00 +0600

    check is this user addmin

--- index.js
+++ index.js
@@ -60,6 +60,19 @@ async function run() {
             const result = await userCollection.find().toArray();
             res.send(result);
         })
+        app.get('/users/admin/:email',verifyToken, async(req,res)=>{
+            const email = req.params.email;
+            if(email === req.decoded.email){
+                return res.status(403).send({message: 'unauthorized access'});
+            }
+            const query = {email:email};
+            const user = await userCollection.findOne(query);
+            let admin = '';
+            if(user){
+                admin = user?.role === 'admin';
+            }
+            res.send({admin});
+        })
         app.post('/users', async (req, res) => {
             const user = req.body;
             //inset email if user does not exists.
